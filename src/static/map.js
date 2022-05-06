c670c9aee5ae400507ce8501d64fd7118e11cde3
kevin-rono <kevin.rono@yale.edu>
2022-05-06 11:20:31 -0400

    added mappings for locations

--- src/static/map.js
+++ src/static/map.js
@@ -1,3 +1,24 @@
+const locations = {
+  L7: "Along the Wall of the Synagogue",
+  "T. 22":
+    "Four meters above the floor in Tower 22, just south of the Mithraeum, in Block J8",
+  "Wall along T. 3":
+    "North Wall in the area of the Temple of Azzanathkona and Tower 3",
+  "N MG": "Northeast Tower of the Main Gate",
+  "No location": "Not available",
+  "L7-L8": "Wall Street back of Blocks L7 or L8",
+  "E7 W13": "Temple of Artemis Azzanathkona",
+  "South MG": "Southwest tower of Main Gate",
+  J9: "Temple of Bel",
+  "T. 19": "Tower 19",
+  "T. 2": "Tower 2",
+  L7: "Wall Street back of Block L7",
+  L8: "Wall Street back of Block L8",
+  "Wall MG": "Wall Street near the Main Gate",
+};
+
+const coordinates = {};
+
 $("#image").click(function (e) {
   let x = e.pageX - this.offsetLeft;
   let y = e.pageY - this.offsetTop;
