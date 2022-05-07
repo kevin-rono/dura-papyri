5e343d341df4a924aa7d8c1d8424938ba622142c
Alina Kramp <alinakramp@Alinas-MacBook-Pro.local>
2022-05-07 10:14:41 -0400

    minor updates on frontend

--- src/static/timeline.js
+++ src/static/timeline.js
@@ -1,10 +1,15 @@
+
 function showTimeline() {
   var start = document.getElementById("start").value;
   var end = document.getElementById("end").value;
 
   console.log(start);
 
+  // error handling
   if (start > end) {
     alert("Start date must be before end date");
+    return;
   }
+
+  
 }
