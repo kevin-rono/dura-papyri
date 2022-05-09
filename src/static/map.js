c2601a0b42b72686609113d73bc209aca75796b5
kevin-rono <kevin.rono@yale.edu>
2022-05-09 01:43:12 -0400

    map updates

--- src/static/map.js
+++ src/static/map.js
@@ -1,40 +1,10 @@
-
 $("#image").click(function (e) {
   alert(
     "No papyri found in this area of Dura-Europos; click a colored area to explore!"
   );
 });
 
-<<<<<<< HEAD
-// for each id in map
-// create fetch, passing the the value of the id in locations
-// example: L7 -> locations[L7]
-// response -> data
-// attach data to results.html
-
-function populateResults(id) {
-  // let test = $("<div>").html("test");
-  // $("#test").append(test);
-
-  $("#test").html("test");
-
-  console.log(id);
-
-  fetch("/display_results", {
-    method: "GET",
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      data = locations[id];
-      console.log(data);
-    });
-}
-=======
 function storeLocation(location) {
-  localStorage.setItem('location', location);
-  console.log(localStorage.getItem('location'));
-
+  localStorage.setItem("location", location);
+  console.log(localStorage.getItem("location"));
 }
-
-
->>>>>>> 24e75e3779b568f5e6cb193cf339128e16b20e92
--- src/static/results.js
+++ src/static/results.js
@@ -1,39 +1,35 @@
-
 // location dictionary based on Findspots
 const locations = {
-    L7: ["Along the Wall of the Synagogue", "Block L7", "Blocks L7"],
-    L8: ["Block L8", "Blocks L7", "Block L7 or L8"],
-    "L7-L8": ["Blocks L7", "Block L7 or L8"],
-    "T. 22": "Tower 22",
-    "Wall along T. 3": "Tower 3",
-    "N MG": "Northeast Tower of the Main Gate",
-    "No location": ["Not available", "Unclear", "Unknown"],
-    "E7 W13": ["Temple of Artemis Azzanathkona", "Temple of Azzanathkona"],
-    "South MG": "Southwest",
-    J9: "Temple of Bel",
-    "T. 19": "Tower 19",
-    "T. 2": "Tower 2",
-    "Wall MG": "Wall Street near the Main Gate",
-  };
+  L7: ["Along the Wall of the Synagogue", "Block L7", "Blocks L7"],
+  L8: ["Block L8", "Blocks L7", "Block L7 or L8"],
+  "L7-L8": ["Blocks L7", "Block L7 or L8"],
+  "T. 22": "Tower 22",
+  "Wall along T. 3": "Tower 3",
+  "N MG": "Northeast Tower of the Main Gate",
+  "No location": ["Not available", "Unclear", "Unknown"],
+  "E7 W13": ["Temple of Artemis Azzanathkona", "Temple of Azzanathkona"],
+  "South MG": "Southwest",
+  J9: "Temple of Bel",
+  "T. 19": "Tower 19",
+  "T. 2": "Tower 2",
+  "Wall MG": "Wall Street near the Main Gate",
+};
 
+window.onload = function () {
+  let location = localStorage.getItem("location");
+  console.log(location);
 
-window.onload = function() {
+  // fetch locations
+  let form = new FormData();
+  form.append("location", locations[location]);
 
-    let location = localStorage.getItem('location');
-    console.log(location);
-
-    // fetch locations
-    let form = new FormData();
-    form.append("location", locations[location]);
-
-    fetch("/display_results", { 
-      method: "POST", 
-      body: form,
-    })
-    .then(response => response.json())
-    .then(data => { 
+  fetch("/display_results", {
+    method: "POST",
+    body: form,
+  })
+    .then((response) => response.json())
+    .then((data) => {
       data = data.data;
-  });
-  
-  }
-
+      console.log(data);
+    });
+};
